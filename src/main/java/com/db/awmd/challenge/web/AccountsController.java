2b056e4d787ab32b7294cc5b6c3bb68ef251fe29
manjunatha.m <manjunatha.m@gmail.com>
2021-02-23 17:07:21 +0530

    added maven dependency

--- src/main/java/com/db/awmd/challenge/web/AccountsController.java
+++ src/main/java/com/db/awmd/challenge/web/AccountsController.java
@@ -5,17 +5,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
 @RequestMapping("/v1/api")
-@Slf4j
 public class AccountsController {
 
 	@GetMapping(path = "/wish/{name}")
 	public String  getAccount(@PathVariable String name) {
-		log.info(name);
-		return "hi" + name;
+		return "hi " + name ;
 	}
 
 }
